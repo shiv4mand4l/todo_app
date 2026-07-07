38e630f94433fbe805e6ff4a715ef986f414ea34
Shivam Mandal <mandalshiva444@gmail.com>
2026-07-07 17:42:26 +0545

    intregrating google register and getting sign in outcome

--- lib/Screens/google_login_screen.dart
+++ lib/Screens/google_login_screen.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:printing/printing.dart';
 
 class GoogleLoginScreen extends StatefulWidget {
   const GoogleLoginScreen({super.key});
@@ -9,14 +12,62 @@ class GoogleLoginScreen extends StatefulWidget {
 }
 
 class _GoogleLoginScreenState extends State<GoogleLoginScreen> {
+  // firebase auth then google sign in
+  String token = 'lodaing ...';
+  bool isLoading = false;
+
+  User? _user;
+  String? _error = '';
+
   @override
   void initState() {
     getFCMToken();
     super.initState();
+    FirebaseAuth.instance.authStateChanges().listen((user) {
+      setState(() {
+        _user = user;
+      });
+    });
+
+    GoogleSignIn.instance.initialize();
   }
 
-  // firebase auth then google sign in
-  String token = 'lodaing ...';
+  // Firebase User login
+  Future<void> login() async {
+    try {
+      setState(() {
+        isLoading = true;
+      });
+      // when user click in login button
+      final GoogleSignInAccount googleUser = await GoogleSignIn.instance
+          .authenticate();
+
+      // ignore: dead_code
+      if (googleUser == null) {
+        // cancel by user
+      }
+
+      // step 2
+      final googleAuth = googleUser?.authentication;
+
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth!.idToken,
+      );
+      await FirebaseAuth.instance.signInWithCredential(credential);
+    } catch (e) {
+      // print(e);
+      _error = e.toString();
+    } finally {
+      setState(() {
+        isLoading = false;
+      });
+    }
+  }
+
+  // Firebase User logout
+  Future<void> logout() async {
+    GoogleSignIn.instance.signOut();
+  }
 
   // @override
   // void initState() {
@@ -29,17 +80,25 @@ class _GoogleLoginScreenState extends State<GoogleLoginScreen> {
     return Scaffold(
       appBar: AppBar(title: Text('Google Login Screen'), centerTitle: true),
       body: Column(
-        crossAxisAlignment: .center,
-        spacing: 50,
+        // spacing: 50,
         children: [
-          ElevatedButton(
-            onPressed: () {
-              // google sign in
-            },
-            child: Text('Generate FCM Token'),
+          isLoading
+              ? CircularProgressIndicator()
+              : ElevatedButton(
+                  onPressed: () {
+                    // google sign in
+                    _user == null ? login() : logout();
+                  },
+                  child: Text('Google Login'),
+                ),
+          Text('User name: ${_user?.displayName ?? 'n/a'}'),
+          Text('Email: ${_user?.email ?? 'n/a'} '),
+          Text('Email: ${_user?.emailVerified ?? 'n/a'} '),
+          // Text(_user?.phoneNumber ?? 'n/a'),
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: NetworkImage(_user?.photoURL ?? 'Nothimg'),
           ),
-          Text('User name:'),
-          Text('Email:'),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0),
             // child: Text('FCM Token is : "$token"'),
